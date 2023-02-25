d1be9adec44041775c540a382d83a9c41ef19e6c
bca <bca@Bcalab4-m15>
2023-02-25 09:09:17 +0530

    hii exam

---
+++ Examisa/src/WaterBill.java
@@ -0,0 +1,20 @@
+
+public class WaterBill {
+	String Acct_num;
+	int num_units;
+	double unit_cost;
+	double bill_amt;
+	
+	WaterBill(String Acct_num,int num_units, double unit_cost)
+	{
+		this.Acct_num = Acct_num;
+		this.num_units = num_units;
+		this.unit_cost = unit_cost;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
